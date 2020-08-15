fc80eb3d54561e24e964d46a60d016267996367d
Filip Š <filip.stamcar@hotmail.com>
2020-08-15 17:57:05 +0200

    Add example app

--- pychess/__main__.py
+++ pychess/__main__.py
@@ -1,11 +1,22 @@
-import sys
-
-from . import hello
+import pygame
 
 
 def main():
-    name = sys.argv[1] if len(sys.argv) > 1 else 'world'
-    hello(name)
+    pygame.init()
+
+    screen = pygame.display.set_mode([500, 500])
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((255, 255, 255))
+        pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+        pygame.display.flip()
+
+    pygame.quit()
 
 
 if __name__ == '__main__':
