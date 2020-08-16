62b02de9a8111a9263bb9c72b1bf8369be488b02
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2020-08-16 09:38:14 +0200

    Update pieces.py

--- pychess/pieces.py
+++ pychess/pieces.py
@@ -1,13 +1,22 @@
 from numpy import full
+from abc import abstractmethod, ABC
 
 
 class Piece:
-    def __init__(self, x, y):
+    def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
 
+    @abstractmethod
     def scan_board(self):
         pass
 
+
+class King(Piece):
+    def __init__(self, x: int, y: int):
+        super().__init__(x, y)
+
+    def scan_board(self):
+        pass
 
 # scan_board(): return full((8, 8), False)
