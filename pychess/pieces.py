4d0bdd3af4db574de9cdbdd3bc21afab96ed6337
Filip Š <filip.stamcar@hotmail.com>
2020-08-18 21:10:15 +0200

    Improve evaluation function

--- pychess/pieces.py
+++ pychess/pieces.py
@@ -12,7 +12,12 @@ class Piece:
     texture = pygame.image.load(path.dirname(__file__) + '/resources/pieces.png')
 
     texture_y = None
+
     _weight = None
+    _weights = None
+
+    _black_weights = None
+    _white_weights = None
 
     def __init__(self, x: int, y: int, black: bool):
         self.x = x
@@ -41,7 +46,14 @@ class Piece:
 
     @property
     def weight(self):
-        return -self._weight if self.black else self._weight
+        if not self._black_weights and self.black:
+            self._black_weights = self._weights[::-1]
+        if not self._white_weights and not self.black:
+            self._white_weights = self._weights
+
+        base_weight = -self._weight if self.black else self._weight
+        position_weight = self._black_weights[self.y][self.x] if self.black else self._white_weights[self.y][self.x]
+        return base_weight + position_weight
 
     def render(self, screen):
         screen.blit(self.texture, (
@@ -58,7 +70,18 @@ class Piece:
 
 class King(Piece):
     texture_y = 0
-    _weight = 90
+
+    _weight = 900
+    _weights = [
+        [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+        [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+        [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+        [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+        [-2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0],
+        [-1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0],
+        [2.0,  2.0,  0.0,  0.0,  0.0,  0.0,  2.0,  2.0],
+        [2.0,  3.0,  1.0,  0.0,  0.0,  1.0,  3.0,  2.0],
+    ]
 
     def __init__(self, x: int, y: int, black: bool):
         super().__init__(x, y, black)
@@ -97,7 +120,18 @@ class King(Piece):
 
 class Queen(Piece):
     texture_y = 1
-    _weight = 9
+
+    _weight = 90
+    _weights = [
+        [-2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0],
+        [-1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
+        [-1.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
+        [-0.5,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
+        [0.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
+        [-1.0,  0.5,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
+        [-1.0,  0.0,  0.5,  0.0,  0.0,  0.0,  0.0, -1.0],
+        [-2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0],
+    ]
 
     def update_board(self, ignore_king=False):
         self._saved_board = full((8, 8), False)
@@ -125,7 +159,18 @@ class Queen(Piece):
 
 class Rook(Piece):
     texture_y = 4
-    _weight = 5
+
+    _weight = 50
+    _weights = [
+        [0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
+        [0.5,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  0.5],
+        [-0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+        [-0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+        [-0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+        [-0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+        [-0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+        [0.0,   0.0, 0.0,  0.5,  0.5,  0.0,  0.0,  0.0],
+    ]
 
     def update_board(self, ignore_king=False):
         self._saved_board = full((8, 8), False)
@@ -152,7 +197,18 @@ class Rook(Piece):
 
 class Bishop(Piece):
     texture_y = 2
-    _weight = 3
+
+    _weight = 30
+    _weights = [
+        [-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0],
+        [-1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
+        [-1.0,  0.0,  0.5,  1.0,  1.0,  0.5,  0.0, -1.0],
+        [-1.0,  0.5,  0.5,  1.0,  1.0,  0.5,  0.5, -1.0],
+        [-1.0,  0.0,  1.0,  1.0,  1.0,  1.0,  0.0, -1.0],
+        [-1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0, -1.0],
+        [-1.0,  0.5,  0.0,  0.0,  0.0,  0.0,  0.5, -1.0],
+        [-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0],
+    ]
 
     def update_board(self, ignore_king=False):
         self._saved_board = full((8, 8), False)
@@ -179,7 +235,18 @@ class Bishop(Piece):
 
 class Knight(Piece):
     texture_y = 3
-    _weight = 3
+
+    _weight = 30
+    _weights = [
+        [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0],
+        [-4.0, -2.0,  0.0,  0.0,  0.0,  0.0, -2.0, -4.0],
+        [-3.0,  0.0,  1.0,  1.5,  1.5,  1.0,  0.0, -3.0],
+        [-3.0,  0.5,  1.5,  2.0,  2.0,  1.5,  0.5, -3.0],
+        [-3.0,  0.0,  1.5,  2.0,  2.0,  1.5,  0.0, -3.0],
+        [-3.0,  0.5,  1.0,  1.5,  1.5,  1.0,  0.5, -3.0],
+        [-4.0, -2.0,  0.0,  0.5,  0.5,  0.0, -2.0, -4.0],
+        [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0],
+    ]
 
     def update_board(self, ignore_king=False):
         self._saved_board = full((8, 8), False)
@@ -200,7 +267,18 @@ class Knight(Piece):
 
 class Pawn(Piece):
     texture_y = 5
-    _weight = 1
+
+    _weight = 10
+    _weights = [
+        [0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
+        [5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0],
+        [1.0,  1.0,  2.0,  3.0,  3.0,  2.0,  1.0,  1.0],
+        [0.5,  0.5,  1.0,  2.5,  2.5,  1.0,  0.5,  0.5],
+        [0.0,  0.0,  0.0,  2.0,  2.0,  0.0,  0.0,  0.0],
+        [0.5, -0.5, -1.0,  0.0,  0.0, -1.0, -0.5,  0.5],
+        [0.5,  1.0, 1.0,  -2.0, -2.0,  1.0,  1.0,  0.5],
+        [0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
+    ]
 
     def update_board(self, ignore_king=False):
         self._saved_board = full((8, 8), False)
