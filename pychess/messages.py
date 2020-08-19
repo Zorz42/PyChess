67663c54d15a15a34b25a032bd9d0322e927a998
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2020-08-19 17:25:19 +0200

    slightly improved messages

--- pychess/messages.py
+++ pychess/messages.py
@@ -11,15 +11,15 @@ def messages_init():
     global win_text, win_text_rect, lose_text, lose_text_rect, draw_text, draw_text_rect
     pygame.font.init()
     font = pygame.font.Font('freesansbold.ttf', 50)
-    win_text = font.render('You Win!', True, (11, 218, 81))
+    win_text = font.render('You Won!', True, (11, 218, 81))
     win_text_rect = win_text.get_rect()
     win_text_rect.center = window_padding + cell_size * 4, window_padding + cell_size * 4
 
-    lose_text = font.render('You Lose!', True, (184, 15, 10))
+    lose_text = font.render('You Lost!', True, (184, 15, 10))
     lose_text_rect = lose_text.get_rect()
     lose_text_rect.center = window_padding + cell_size * 4, window_padding + cell_size * 4
 
-    draw_text = font.render('Draw!', True, (220, 220, 0))
+    draw_text = font.render('Game Draw!', True, (220, 220, 0))
     draw_text_rect = draw_text.get_rect()
     draw_text_rect.center = window_padding + cell_size * 4, window_padding + cell_size * 4
 
