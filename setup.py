6f284bf066d96ebf0a04010396f80fa52f0be898
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2020-08-21 18:49:54 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    def cythonize(*args, **kwargs):
+        return []
+
+extensions = cythonize([
+    'pychess/__init__.py',
+    'pychess/algorithm.py',
+    'pychess/main.py',
+    'pychess/messages.py',
+    'pychess/pieces.py',
+    'pychess/renderers.py',
+    'pychess/speech.py',
+    'pychess/util.py',
+    'pychess/variables.py',
+])
+
+for extension in extensions:
+    extension.optional = True
+
+setup(
+    name='PyChess',
+    description='Chess game and AI using PyGame',
+
+    packages=['pychess'],
+    ext_modules=extensions,
+
+    entry_points={'console_scripts': ['pychess=pychess.__main__:main']},
+
+    setup_requires=[
+        'cython>=3.0.0a,<4.0.0',
+    ],
+    install_requires=[
+        'pygame>=2.0.0<3.0.0',
+        'numpy>=1.0.0<2.0.0',
+        'pyttsx3>=2.0.0<3.0.0'
+    ],
+)
